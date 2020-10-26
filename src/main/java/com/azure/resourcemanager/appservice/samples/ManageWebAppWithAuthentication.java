2587e719a8baa13ca3a5f7c218ad3e5381cdbd38
Azure-Fluent <azfluent@microsoft.com>
2020-10-26 13:02:28 +0800

    Release azure-resourcemanager 2.0.0

--- src/main/java/com/azure/resourcemanager/appservice/samples/ManageWebAppWithAuthentication.java
+++ src/main/java/com/azure/resourcemanager/appservice/samples/ManageWebAppWithAuthentication.java
@@ -1,29 +1,24 @@
-/**
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for
- * license information.
- */
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
-package com.microsoft.azure.management.appservice.samples;
+package com.azure.resourcemanager.appservice.samples;
 
-import com.microsoft.azure.management.Azure;
-import com.microsoft.azure.management.appservice.AppServicePlan;
-import com.microsoft.azure.management.appservice.BuiltInAuthenticationProvider;
-import com.microsoft.azure.management.appservice.JavaVersion;
-import com.microsoft.azure.management.appservice.PricingTier;
-import com.microsoft.azure.management.appservice.WebApp;
-import com.microsoft.azure.management.appservice.WebContainer;
-import com.microsoft.azure.management.resources.fluentcore.arm.Region;
-import com.microsoft.azure.management.resources.fluentcore.utils.SdkContext;
-import com.microsoft.azure.management.samples.Utils;
-import com.microsoft.rest.LogLevel;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.management.AzureEnvironment;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.resourcemanager.AzureResourceManager;
+import com.azure.resourcemanager.appservice.models.AppServicePlan;
+import com.azure.resourcemanager.appservice.models.BuiltInAuthenticationProvider;
+import com.azure.resourcemanager.appservice.models.JavaVersion;
+import com.azure.resourcemanager.appservice.models.PricingTier;
+import com.azure.resourcemanager.appservice.models.WebApp;
+import com.azure.resourcemanager.appservice.models.WebContainer;
+import com.azure.core.management.Region;
+import com.azure.core.management.profile.AzureProfile;
+import com.azure.resourcemanager.samples.Utils;
+import com.azure.core.http.policy.HttpLogDetailLevel;
 
 import java.io.Console;
-import java.io.File;
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Azure App Service sample for managing authentication for web apps.
@@ -35,25 +30,23 @@ import java.util.concurrent.TimeUnit;
  */
 public final class ManageWebAppWithAuthentication {
 
-    private static OkHttpClient httpClient;
-
     /**
      * Main function which runs the actual sample.
-     * @param azure instance of the azure client
+     * @param azureResourceManager instance of the azure client
      * @return true if sample runs successfully
      */
-    public static boolean runSample(Azure azure) {
+    public static boolean runSample(AzureResourceManager azureResourceManager) {
         // New resources
         final String suffix         = ".azurewebsites.net";
-        final String app1Name       = SdkContext.randomResourceName("webapp1-", 20);
-        final String app2Name       = SdkContext.randomResourceName("webapp2-", 20);
-        final String app3Name       = SdkContext.randomResourceName("webapp3-", 20);
-        final String app4Name       = SdkContext.randomResourceName("webapp4-", 20);
+        final String app1Name       = Utils.randomResourceName(azureResourceManager, "webapp1-", 20);
+        final String app2Name       = Utils.randomResourceName(azureResourceManager, "webapp2-", 20);
+        final String app3Name       = Utils.randomResourceName(azureResourceManager, "webapp3-", 20);
+        final String app4Name       = Utils.randomResourceName(azureResourceManager, "webapp4-", 20);
         final String app1Url        = app1Name + suffix;
         final String app2Url        = app2Name + suffix;
         final String app3Url        = app3Name + suffix;
         final String app4Url        = app4Name + suffix;
-        final String rgName         = SdkContext.randomResourceName("rg1NEMV_", 24);
+        final String rgName         = Utils.randomResourceName(azureResourceManager, "rg1NEMV_", 24);
 
         try {
 
@@ -63,7 +56,7 @@ public final class ManageWebAppWithAuthentication {
 
             System.out.println("Creating web app " + app1Name + " in resource group " + rgName + "...");
 
-            WebApp app1 = azure.webApps().define(app1Name)
+            WebApp app1 = azureResourceManager.webApps().define(app1Name)
                     .withRegion(Region.US_WEST)
                     .withNewResourceGroup(rgName)
                     .withNewWindowsPlan(PricingTier.STANDARD_S1)
@@ -100,8 +93,8 @@ public final class ManageWebAppWithAuthentication {
             // Create a second web app
 
             System.out.println("Creating another web app " + app2Name + " in resource group " + rgName + "...");
-            AppServicePlan plan = azure.appServices().appServicePlans().getById(app1.appServicePlanId());
-            WebApp app2 = azure.webApps().define(app2Name)
+            AppServicePlan plan = azureResourceManager.appServicePlans().getById(app1.appServicePlanId());
+            WebApp app2 = azureResourceManager.webApps().define(app2Name)
                     .withExistingWindowsPlan(plan)
                     .withExistingResourceGroup(rgName)
                     .withJavaVersion(JavaVersion.JAVA_8_NEWEST)
@@ -136,7 +129,7 @@ public final class ManageWebAppWithAuthentication {
             // Create a 3rd web app with a public GitHub repo in Azure-Samples
 
             System.out.println("Creating another web app " + app3Name + "...");
-            WebApp app3 = azure.webApps().define(app3Name)
+            WebApp app3 = azureResourceManager.webApps().define(app3Name)
                     .withExistingWindowsPlan(plan)
                     .withNewResourceGroup(rgName)
                     .defineSourceControl()
@@ -173,7 +166,7 @@ public final class ManageWebAppWithAuthentication {
             // Create a 4th web app
 
             System.out.println("Creating another web app " + app4Name + "...");
-            WebApp app4 = azure.webApps()
+            WebApp app4 = azureResourceManager.webApps()
                     .define(app4Name)
                     .withExistingWindowsPlan(plan)
                     .withExistingResourceGroup(rgName)
@@ -204,13 +197,10 @@ public final class ManageWebAppWithAuthentication {
             Utils.print(app4);
 
             return true;
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-            e.printStackTrace();
         } finally {
             try {
                 System.out.println("Deleting Resource Group: " + rgName);
-                azure.resourceGroups().beginDeleteByName(rgName);
+                azureResourceManager.resourceGroups().beginDeleteByName(rgName);
                 System.out.println("Deleted Resource Group: " + rgName);
             } catch (NullPointerException npe) {
                 System.out.println("Did not create any resources in Azure. No clean up is necessary");
@@ -218,7 +208,6 @@ public final class ManageWebAppWithAuthentication {
                 g.printStackTrace();
             }
         }
-        return false;
     }
     /**
      * Main entry point.
@@ -230,34 +219,24 @@ public final class ManageWebAppWithAuthentication {
             //=============================================================
             // Authenticate
 
-            final File credFile = new File(System.getenv("AZURE_AUTH_LOCATION"));
+            final AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
+            final TokenCredential credential = new DefaultAzureCredentialBuilder()
+                .authorityHost(profile.getEnvironment().getActiveDirectoryEndpoint())
+                .build();
 
-            Azure azure = Azure
-                    .configure()
-                    .withLogLevel(LogLevel.BASIC)
-                    .authenticate(credFile)
-                    .withDefaultSubscription();
+            AzureResourceManager azureResourceManager = AzureResourceManager
+                .configure()
+                .withLogLevel(HttpLogDetailLevel.BASIC)
+                .authenticate(credential, profile)
+                .withDefaultSubscription();
 
             // Print selected subscription
-            System.out.println("Selected subscription: " + azure.subscriptionId());
+            System.out.println("Selected subscription: " + azureResourceManager.subscriptionId());
 
-            runSample(azure);
+            runSample(azureResourceManager);
         } catch (Exception e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
-    }
-
-    private static String curl(String url) {
-        Request request = new Request.Builder().url(url).get().build();
-        try {
-            return httpClient.newCall(request).execute().body().string();
-        } catch (IOException e) {
-            return null;
-        }
-    }
-
-    static {
-        httpClient = new OkHttpClient.Builder().readTimeout(1, TimeUnit.MINUTES).build();
     }
 }
